12140ed38fecc01c11f472dc01b959ed0c5bf9b6
YI YING <supercarboy88@gmail.com>
2020-01-08 08:07:04 -0800

    deleted the route folder

--- server/server.jsx
+++ server/server.jsx
@@ -13,11 +13,6 @@ const users = require('./routes/users');
 
 const app = express();
 
-
-// -----------------------------------------------
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
