14fba8df5a1c808419d43c6322447b8ade81e900
Emilio Valladares <vallae.wumbo@gmail.com>
2020-01-11 11:51:18 -0800

    Basic Styling.

--- screens/SignUpScreen.jsx
+++ screens/SignUpScreen.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
-
+import { StyleSheet, View, TextInput, Button, ScrollView } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import Logo from '../components/Logo';
 
 export default class SignUpScreen extends Component {
 
@@ -9,44 +9,47 @@ export default class SignUpScreen extends Component {
         return (
             <View
                 style={styles.container}>
-                <View
-                    style={styles.container}>
-                    <TextInput style={styles.inputBox}
-                        placeholder="First"
-                        placeholderTextColor="#fff"
-                        selectionColor="#fff"
-                        keyboardType="email-address"
-                        keyboardAppearance='dark'
-                    />
-                    <TextInput style={styles.inputBox}
-                        placeholder="Last"
-                        placeholderTextColor="#fff"
-                        selectionColor="#fff"
-                        keyboardType="email-address"
-                        keyboardAppearance='dark'
-                    />
-
-                    {/* this needs to be changed to the upload file button */}
-                    <TextInput style={styles.inputBox}
-                        placeholder="Image"
-                        placeholderTextColor="#fff"
-                        secureTextEntry={true}
-                        keyboardAppearance='dark'
-                    />
-                    <TextInput style={styles.inputBox}
-                        placeholder="Email"
-                        placeholderTextColor="#fff"
-                        selectionColor="#fff"
-                        keyboardType="email-address"
-                        keyboardAppearance='dark'
-                    />
-                    <TextInput style={styles.inputBox}
-                        placeholder="Password"
-                        placeholderTextColor="#fff"
-                        secureTextEntry={true}
-                        keyboardAppearance='dark'
-                    />
-                </View>
+                <ScrollView>
+                    <Logo />
+                    <View
+                        style={styles.container}>
+                        <TextInput style={styles.inputBox}
+                            placeholder="First"
+                            placeholderTextColor="#fff"
+                            selectionColor="#fff"
+                            keyboardType="email-address"
+                            keyboardAppearance='dark'
+                        />
+                        <TextInput style={styles.inputBox}
+                            placeholder="Last"
+                            placeholderTextColor="#fff"
+                            selectionColor="#fff"
+                            keyboardType="email-address"
+                            keyboardAppearance='dark'
+                        />
+                        <TextInput style={styles.inputBox}
+                            placeholder="Email"
+                            placeholderTextColor="#fff"
+                            selectionColor="#fff"
+                            keyboardType="email-address"
+                            keyboardAppearance='dark'
+                        />
+                        <TextInput style={styles.inputBox}
+                            placeholder="Password"
+                            placeholderTextColor="#fff"
+                            secureTextEntry={true}
+                            keyboardAppearance='dark'
+                        />
+                        <View style={styles.buttonsContainer}>
+                            <View style={styles.button}>
+                                <Button
+                                    color="#ffffff"
+                                    title="Submit"
+                                />
+                            </View>
+                        </View>
+                    </View>
+                </ScrollView>
             </View>
         );
     };
@@ -60,21 +63,10 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    signupContainer: {
-        flexGrow: 1,
-        alignItems: 'flex-end',
-        justifyContent: 'center',
-        paddingVertical: 16,
-        flexDirection: 'row',
-    },
+
     signupText: {
         color: 'rgba(255, 255, 255, 0.6)',
         fontSize: 16,
-    },
-    signupButton: {
-        color: '#fff',
-        fontSize: 16,
-        fontWeight: '500',
     },
 
     inputBox: {
@@ -86,5 +78,16 @@ const styles = StyleSheet.create({
         fontSize: 30,
         color: '#fff',
         marginVertical: 10,
+    },
+
+    buttonsContainer: {
+        justifyContent: 'center',
+        flexDirection: 'row'
+    },
+
+    button: {
+        backgroundColor: 'black',
+        margin: 10,
+        fontWeight: '500'
     }
 });
