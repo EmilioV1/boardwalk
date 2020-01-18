a4c28293605aeb3d3484857543cfba12f997be78
Neha Nehru <neha_nehru@hotmail.com>
2020-01-18 10:33:14 -0800

    Added calculators in new debt screen

--- screens/NewDebt.jsx
+++ screens/NewDebt.jsx
@@ -2,15 +2,49 @@ import React from 'react';
 import { Actions } from 'react-native-router-flux';
 import { StyleSheet, View, Text } from 'react-native';
 
-export default function NewDebt() {
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.newDebtText}>Hello, I am the new debt screen!</Text>
-    </View>
-  );
+export default class nDebtCalculator extends Component {
+  state = {
+    amountValue: "5000",
+    yearsValue: "1",
+    interestValue: "0.5"
+  };
 
-};
+  handleAmountChange = value => {
+    this.setState({ amountValue: value });
+  };
+  handleYearChange = value => {
+    this.setState({ yearsValue: value });
+  };
+
+  render() {
+    const { amountValue, yearsValue } = this.state;
+
+    return (
+      <div className="App">
+        <h4>I want to borrow £{amountValue}</h4>
+        <InputRange
+          step={100}
+          maxValue={20000}
+          minValue={1000}
+          value={amountValue}
+          onChange={this.handleAmountChange}
+        />
+        <h4>
+          Over {yearsValue} year{yearsValue > 1 && "s"}
+        </h4>
+        <InputRange
+          step={interestValue}
+          maxValue={30}
+          minValue={1}
+          value={yearsValue}
+          onChange={this.handleYearChange}
+        />
+        <Display years={yearsValue} amount={amountValue} />
+      </div>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
 
